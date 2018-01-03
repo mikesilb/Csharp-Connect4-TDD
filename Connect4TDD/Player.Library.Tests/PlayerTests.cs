dc0d5440f2be603b20ff7caea2bcc4893cd43b31
Michael Silberstein <mikesilb123@gmail.com>
2018-01-03 10:08:08 -0500

    Completed 'Player' functionality with passing tests

--- Connect4TDD/Player.Library.Tests/PlayerTests.cs
+++ Connect4TDD/Player.Library.Tests/PlayerTests.cs
@@ -6,8 +6,40 @@ namespace PlayerLibrary.Tests
     public class PlayerTests
     {
         [Test]
-        public void TestCase()
+        public void TestPlayerConstructor()
         {
+            Player john = new Player("John", 12, 8.5, 21);
+            Player computer = new Player("MyMacComputer", 3, 9.99, 21);
+
+            Assert.AreEqual(john.GetName(), "John");
+            Assert.AreEqual(john.GetAge(), 12);
+            Assert.AreEqual(john.GetSkillLevel(), 8.5);
+            Assert.AreEqual(john.GetNumChipsRemain(), 21);
+            Assert.AreEqual(computer.GetName(), "MyMacComputer");
+            Assert.AreEqual(computer.GetAge(), 3);
+            Assert.AreEqual(computer.GetSkillLevel(), 9.99);
+            Assert.AreEqual(computer.GetNumChipsRemain(), 21);
         }
+
+        [Test]
+        public void TestDecreaseChips()
+        {
+            Player john = new Player("John", 12, 8.5, 21);
+            Player computer = new Player("MyMacComputer", 3, 9.99, 21);
+            john.DecreaseChips();
+            Assert.AreEqual(john.GetNumChipsRemain(), 20);
+            computer.DecreaseChips();
+            Assert.AreEqual(computer.GetNumChipsRemain(), 20);
+
+            for (int i = 1; i <= 3; i++)
+            {
+                john.DecreaseChips();
+            }
+            Assert.AreEqual(john.GetNumChipsRemain(), 17);
+        }
+
     }
 }
+
+    
+
