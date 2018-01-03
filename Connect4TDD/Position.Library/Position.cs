d9cbaf1b5d1faad9d76a5763f67bd4e7bd3b72b4
Michael Silberstein <mikesilb123@gmail.com>
2018-01-03 13:45:24 -0500

    Completed 'Position' functionality with passing tests

--- Connect4TDD/Position.Library/Position.cs
+++ Connect4TDD/Position.Library/Position.cs
@@ -1,8 +1,55 @@
 ﻿using System;
+using ChipLibrary;
+
 namespace PositionLibrary
 {
     public class Position
     {
-       
+        private int[] _location;
+        private bool _availability;
+        private Chip _chipAtPosition;
+
+        public Position(int[] location, bool availability, Chip chipAtPosition)
+        {
+            _location = location;
+            _availability = availability;
+            _chipAtPosition = chipAtPosition;
+        }
+
+        public void Occupy(Chip chip)
+        {
+            if (_availability == true)
+            {
+                _availability = false;
+                _chipAtPosition = chip; 
+            }
+
+        }
+
+        public string GetLocation()
+        {
+            return $"[{_location[0]}, {_location[1]}]";
+        }
+
+        public bool GetAvailability()
+        {
+            return _availability;
+        }
+
+        public Chip GetChipAtPosition()
+        {
+            return _chipAtPosition;
+        }
+
+        public void SetAvailability(bool availability)
+        {
+            _availability = availability ;
+        }
+
+        public void GetChipAtPosition(Chip chipAtPosition)
+        {
+            _chipAtPosition = chipAtPosition;
+        }
+
     }
 }
