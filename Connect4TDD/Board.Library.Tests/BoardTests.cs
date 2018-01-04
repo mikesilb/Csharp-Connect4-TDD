03ac03ed060797fea1ebdc98fe7da254476564bc
Michael Silberstein <mikesilb123@gmail.com>
2018-01-04 12:01:15 -0500

    Completed 'Board' constructor with passing tests

--- Connect4TDD/Board.Library.Tests/BoardTests.cs
+++ Connect4TDD/Board.Library.Tests/BoardTests.cs
@@ -1,13 +1,32 @@
 ﻿using NUnit.Framework;
 using System;
+using PlayerLibrary;
+using ChipLibrary;
+using PositionLibrary;
+
 namespace BoardLibrary.Tests
 {
     [TestFixture]
     public class BoardTests
     {
         [Test]
-        public void TestCase()
+        public void TestBoardConstructor()
         {
+
+            Player john = new Player("John", 12, 8.5, 21);
+            Player computer = new Player("MyMacComputer", 3, 9.99, 21);
+            Chip johnChip = new Chip(john.GetName(), 'X');
+            Chip computerChip = new Chip(computer.GetName(), 'O');
+            Board theNewBoard = new Board(7, 6);
+
+            Assert.AreEqual(theNewBoard.GetColumn(), 7);
+            Assert.AreEqual(theNewBoard.GetRow(), 6);
+            Assert.AreEqual(theNewBoard.theGrid.Count, 42);
+            Assert.AreEqual(theNewBoard.theGrid[6].location[0], 6);
+            Assert.AreEqual(theNewBoard.theGrid[6].location[1], 5);
+            Assert.AreEqual(theNewBoard.theGrid[theNewBoard.theGrid.Count - 1].location[0], 6);
+            Assert.AreEqual(theNewBoard.theGrid[theNewBoard.theGrid.Count - 1].location[1], 0);
+    
         }
     }
 }
--- Connect4TDD/Board.Library/Board.cs
+++ Connect4TDD/Board.Library/Board.cs
@@ -1,8 +1,43 @@
 ﻿using System;
+using System.Collections.Generic;
+using PositionLibrary;
+using ChipLibrary;
 namespace BoardLibrary
 {
     public class Board
     {
-      
+        private int _column;
+        private int _row;
+        public List<Position> theGrid = new List<Position>();
+
+        public Board(int column, int row)
+        {
+            _column = column;
+            _row = row;
+            int y = _row - 1;
+            int x;
+
+            while (y >= 0)
+            {
+                x = 0;
+                while (x <= _column - 1)
+                {
+                    theGrid.Add(new Position(new int[] { x, y }, true, null));
+                    x++;
+                }
+                y--;
+            }
+        }
+
+        public int GetColumn()
+        {
+            return _column;
+        }
+
+        public int GetRow()
+        {
+            return _row;
+        }
+
     }
 }
