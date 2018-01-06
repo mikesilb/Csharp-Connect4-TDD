bddc06add9ef71ab67243a5223e304950b3254f2
Michael Silberstein <mikesilb123@gmail.com>
2018-01-06 11:18:30 -0500

    Completed 'Move' functionality with passing tests

--- Connect4TDD/Move.Library/Move.cs
+++ Connect4TDD/Move.Library/Move.cs
@@ -1,8 +1,55 @@
 ﻿using System;
+using ChipLibrary;
+using BoardLibrary;
 namespace MoveLibrary
 {
     public class Move
     {
-       
+        private Chip _chipFromPlayer;
+        private int _column;
+
+        public Move(Chip chipFromPlayer, int column) 
+        {
+            _chipFromPlayer = chipFromPlayer;
+            _column = column;
+        }    
+
+        public Chip GetChipFromPlayer()
+        {
+            return _chipFromPlayer;
+        }
+
+        public int GetColumn()
+        {
+            return _column;
+        }
+
+        public bool? IdentifyDropPosition(Board board)
+        {
+            bool? output = null;
+            if (board.CheckForAvailability(_column, board.GetRow() - 1) == false)
+            {
+                Console.WriteLine("\n\n\nThis column is completely filled.  Make another move.");
+                output = false;
+            }
+            else
+            {
+                int row = 0;
+                while (row <= board.GetRow())
+                {
+                    if (board.CheckForAvailability(_column, row) == true)
+                    {
+                        board.OccupyingThePosition(_column, row, _chipFromPlayer);
+                        output = true;
+                        return output;
+                    }
+                    else
+                    {
+                        row++;
+                    }
+                }
+            }
+            return output;
+        }
     }
 }
